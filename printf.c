8282f1da6ba3ceb8eb8ef43af50beaa117ca1496
lexa9448 <lexa9448@gmail.com>
2022-08-07 12:28:29 +0100

    modified:   printf.c

--- printf.c
+++ printf.c
@@ -2,6 +2,8 @@
 #include <stdarg.h>
 #include <string.h>
 #include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
 
 /**
  * print_char - print a character to stdout
@@ -34,6 +36,39 @@ int print_string(char *str, int count)
 }
 
 /**
+ * print_num - print numbers
+ * @num: number to print
+ * @count: int val to keep counter of numbers printed
+ * Return: count
+ */
+int print_num(int num, int count)
+{
+	int i = 0, len;
+	char last, *ptr2, *ptr = (char *)malloc(sizeof(char));
+
+	if (num < 0)
+		count = print_char('-', count);
+	num = abs(num);
+	while (num != 0)
+	{
+		last = (num % 10) + '0';
+		if (i == 0)
+			*(ptr) = last;
+		else
+		{
+			ptr2 = (char *)realloc(ptr, i + 1);
+			*(ptr2 + i) = last;
+			ptr = ptr2;
+		}
+		num /= 10;
+		i++;
+	}
+	len = strlen(ptr);
+	for (i = len; i > 0; i--)
+		count = print_char(ptr[i - 1], count);
+	return (count);
+}
+/**
  * _printf - print args to stdout
  * @format: arg specifiers
  * Return: char printed
@@ -58,6 +93,8 @@ int _printf(const char *format, ...)
 				count = print_string(va_arg(lst, char *), count);
 			if (next_char == '%')
 				count = print_char('%', count);
+			if (next_char == 'd' || next_char == 'i')
+				count = print_num(va_arg(lst, int), count);
 			i += 2;
 			continue;
 		}
