ebe4e481471892c7813d718e0fe257bde9e1ce17
lexa9448 <lexa9448@gmail.com>
2022-08-08 22:50:17 +0100

    modified:   main.h

--- main.h
+++ main.h
@@ -1,6 +1,8 @@
 #ifndef MAIN_H
 #define MAIN_H
+
 #include <stdarg.h>
+
 int _printf(const char *format, ...);
 int continue_printf(char, va_list, int);
 int print_char(char, int);
@@ -11,4 +13,5 @@ int print_octal(unsigned int, int);
 int print_udecimal(unsigned int, int);
 int print_hex(unsigned int, char, int);
 int print_xstring(char *, int);
+int printf_rot13(va_list, int);
 #endif
