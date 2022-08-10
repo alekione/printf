28daba0589aab710dfefed6a832f6f43dc502e34
Asukwo-comfort <asukwokulex24@gmail.com>
2022-08-10 04:04:40 -0700

    modified:   main.h 	new file:   printf_revstr.c

--- main.h
+++ main.h
@@ -2,6 +2,10 @@
 #define MAIN_H
 
 #include <stdarg.h>
+#include <stddef.h>
+
+#define UNUSED(x) (void)(x)
+#define BUFF_SIZE 1024
 
 int _printf(const char *format, ...);
 int continue_printf(char, va_list);
@@ -16,5 +20,6 @@ int print_xstring(char *);
 int print_sign(va_list, char, char);
 int print_with_hash(va_list, char);
 int printf_rot13(va_list);
+int printf_revstr(va_list types, char buffer[], int flags, int width, int precision, int size);
 
 #endif
